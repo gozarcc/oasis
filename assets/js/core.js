5115c0c54cc61873898a6d63c9efcbd60ec57ec5
gozarcc <gozarcharlene@gmail.com>
2018-05-31 16:18:21 +0800

    Capstone 1 Update

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,5 +1,15 @@
-$(document).ready(function () {
-                 $('#navbar-icon').on('click', function () {
-                     $('#sidebar').toggleClass('active');
-                 });
-             });
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("back-to-top").style.display = "block";
+    } else {
+        document.getElementById("back-to-top").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
